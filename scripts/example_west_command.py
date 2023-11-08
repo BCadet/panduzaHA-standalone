fc145a7e43078698053a06f8367644eb6eb0ed00
Bryan CADET <bryan.cadet@protonmail.com>
2023-11-08 20:49:36 +0100

    compile example mqtt_publisher for rpi_pico

--- scripts/example_west_command.py
+++ scripts/example_west_command.py
@@ -9,12 +9,12 @@ Example of a west extension in the example-application repository.'''
 from west.commands import WestCommand  # your extension must subclass this
 from west import log                   # use this for user output
 
-class ExampleWestCommand(WestCommand):
+class Setup(WestCommand):
 
     def __init__(self):
         super().__init__(
-            'example-west-command',               # gets stored as self.name
-            'an example west extension command',  # self.help
+            'setup',               # gets stored as self.name
+            'setup dependencies for building',  # self.help
             # self.description:
             '''\
 A multi-line description of example-west-command.
@@ -34,8 +34,8 @@ endings.''')
                                          description=self.description)
 
         # Add some example options using the standard argparse module API.
-        parser.add_argument('-o', '--optional', help='an optional argument')
-        parser.add_argument('required', help='a required argument')
+        #parser.add_argument('-o', '--optional', help='an optional argument')
+        #parser.add_argument('required', help='a required argument')
 
         return parser           # gets stored as self.parser
 
@@ -45,5 +45,4 @@ endings.''')
         #   $ west my-command-name -o FOO BAR
         #   --optional is FOO
         #   required is BAR
-        log.inf('--optional is', args.optional)
-        log.inf('required is', args.required)
+        
